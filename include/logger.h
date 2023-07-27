4f49d91fe68198f6a5e03aa66c3c51bb66acf4ec
tfergus23 <tomfergus22@gmail.com>
2023-07-27 14:14:32 -0500

    Finish logger

--- include/logger.h
+++ include/logger.h
@@ -1,17 +1,48 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <vector>
+#include <fstream>
+#include <functional>
 
 namespace tflib{
     enum class log_level {OFF = 0,FATAL = 1,ERROR = 2,WARN = 3,INFO = 4,DEBUG = 5,TRACE = 6};
 
+
+    class std_out_appender{
+    public:
+        log_level max_level;
+        log_level min_level;
+        std_out_appender(log_level level, log_level min_level=log_level::OFF);
+        void log(const std::string& message, log_level log_level);
+    };
+
+    class std_err_appender{
+    public:
+        log_level max_level;
+        log_level min_level;
+        std_err_appender(log_level level, log_level min_level=log_level::OFF);
+        void log(const std::string& message, log_level log_level);
+    };
+
+    class file_appender{
+    public:
+        log_level max_level;
+        log_level min_level;
+        file_appender(const file_appender& that);
+        file_appender(log_level level, const std::string& file_path, log_level min_level=log_level::OFF);
+        void log(const std::string& message, log_level log_level);
+
+    private:
+        const std::string m_file_path;
+        std::ofstream m_ofstream;
+    };
+
     class logger{
     public:
-        std::string get_prefix();
-        void set_prefix(const std::string& prefix);
+        void set_prefix(std::function<std::string(const std::string&)> prefix);
 
-        std::string get_suffix();
-        void set_suffix(const std::string& suffix);
+        void set_suffix(std::function<std::string(const std::string&)> suffix);
 
         void fatal(const std::string& message);
         void error(const std::string& message);
@@ -20,10 +51,17 @@ namespace tflib{
         void debug(const std::string& message);
         void trace(const std::string& message);
 
-        log_level level = log_level::FATAL;
+        void add_std_out_appender(const std_out_appender& appender);
+        void add_std_err_appender(const std_err_appender& appender);
+        void add_file_appender(const file_appender& appender);
+
+        static std::string level_to_string(log_level level);
     private:
-        void log(const std::string& message);
-        std::string m_prefix = "";
-        std::string m_suffix = "";
+        void log(const std::string& message, log_level level);
+        std::function<std::string(const std::string&)> m_prefix = [](const std::string&){return "";};
+        std::function<std::string(const std::string&)> m_suffix = [](const std::string&){return "";};
+        std::vector<std_out_appender> m_std_out_appenders;
+        std::vector<std_err_appender> m_std_err_appenders;
+        std::vector<file_appender> m_file_appenders;
     };
 }
--- tflib/logger.cpp
+++ tflib/logger.cpp
@@ -1,51 +1,93 @@
-#include "include/logger.h"
+#include "../include/logger.h"
 using namespace tflib;
 
-std::string logger::get_prefix(){
-    return m_prefix;
+static const std::unordered_map<log_level, std::string> LEVEL_STRINGS = {
+    {log_level::OFF, "OFF"},
+    {log_level::FATAL, "FATAL"},
+    {log_level::ERROR, "ERROR"},
+    {log_level::WARN, "WARN"},
+    {log_level::INFO, "INFO"},
+    {log_level::DEBUG, "DEBUG"},
+    {log_level::TRACE, "TRACE"},
+};
+
+std_out_appender::std_out_appender(log_level level, log_level min_level) : max_level{level}, min_level{min_level} {}
+void std_out_appender::log(const std::string& message, log_level log_level){
+    if (log_level <= max_level && log_level >= min_level){
+        std::cout << message << '\n';
+    }
 }
-void logger::set_prefix(const std::string& prefix){
+
+std_err_appender::std_err_appender(log_level level, log_level min_level) : max_level{level}, min_level{min_level} {}
+void std_err_appender::log(const std::string& message, log_level log_level){
+    if (log_level <= max_level && log_level >= min_level){
+        std::cerr << message << '\n';
+    }
+}
+
+file_appender::file_appender(const file_appender& that) : m_file_path{that.m_file_path}, max_level{that.max_level}, min_level{that.min_level}{
+    m_ofstream.open(m_file_path, std::ios_base::app);
+}
+file_appender::file_appender(log_level level, const std::string& file_path, log_level min_level) : max_level{level}, m_file_path{file_path}, min_level{min_level}{
+    m_ofstream.open(file_path, std::ios_base::app);
+}
+
+void file_appender::log(const std::string& message, log_level log_level){
+    if (log_level <= max_level && log_level >= min_level){
+        m_ofstream << message << '\n';
+    }
+}
+
+void logger::set_prefix(std::function<std::string(const std::string&)> prefix){
     m_prefix = prefix;
 }
 
-std::string logger::get_suffix(){
-    return m_suffix;
-}
-void logger::set_suffix(const std::string& suffix){
+void logger::set_suffix(std::function<std::string(const std::string&)> suffix){
     m_suffix = suffix;
 }
 
 void logger::fatal(const std::string& message){
-    if (level >= log_level::FATAL){
-        log(message);
-    }
+    log(message, log_level::FATAL);
 }
 void logger::error(const std::string& message){
-    if (level >= log_level::ERROR){
-        log(message);
-    }
+    log(message, log_level::ERROR);
 }
 void logger::warn(const std::string& message){
-    if (level >= log_level::WARN){
-        log(message);
-    }
+    log(message, log_level::WARN);
 }
 void logger::info(const std::string& message){
-    if (level >= log_level::INFO){
-        log(message);
-    }
+    log(message, log_level::INFO);
 }
 void logger::debug(const std::string& message){
-    if (level >= log_level::DEBUG){
-        log(message);
-    }
+    log(message, log_level::DEBUG);
 }
 void logger::trace(const std::string& message){
-    if (level >= log_level::TRACE){
-        log(message);
-    }
+    log(message, log_level::TRACE);
 }
 
-void logger::log(const std::string& message){
-    std::cout << m_prefix << message << m_suffix << '\n';
+void logger::add_std_out_appender(const std_out_appender& appender){
+    m_std_out_appenders.push_back(appender);
+}
+void logger::add_std_err_appender(const std_err_appender& appender){
+    m_std_err_appenders.push_back(appender);
+}
+void logger::add_file_appender(const file_appender& appender){
+    m_file_appenders.push_back(appender);
+}
+
+std::string logger::level_to_string(log_level level){
+    return LEVEL_STRINGS.at(level);
+}
+
+void logger::log(const std::string& message, log_level level){
+    std::string full_message = m_prefix(logger::level_to_string(level)) + message + m_suffix(logger::level_to_string(level));
+    for (auto& appender : m_std_out_appenders){
+        appender.log(full_message, level);
+    }
+    for (auto& appender : m_std_err_appenders){
+        appender.log(full_message, level);
+    }
+    for (auto& appender : m_file_appenders){
+        appender.log(full_message, level);
+    }
 }
