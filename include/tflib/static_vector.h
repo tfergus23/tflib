7f00d2c9023676af9bd6f1d9c6829c99bbe10c02
tfergus23 <tomfergus22@gmail.com>
2023-08-21 13:32:41 -0500

    Add at, find, and remove_at methods to static_vector

--- include/tflib/static_vector.h
+++ include/tflib/static_vector.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <stdexcept>
+#include <cstring>
 
 namespace tflib{
     template <typename T, size_t S>
     class static_vector {
     public:
-        size_t size() {
+        size_t size() const {
             return m_size;
         }
         void push_back(T element) {
@@ -17,14 +18,48 @@ namespace tflib{
         void pop_back() {
             m_size--;
         }
+
+
         T& operator[](size_t index) {
             if (index >= m_size || index < 0) {
                 throw std::out_of_range("Index is out of bounds of the static vector: " + std::to_string(index));
             }
             return arr[index];
         }
+
+        const T at(size_t index) const {
+            if (index >= m_size || index < 0) {
+                throw std::out_of_range("Index is out of bounds of the static vector: " + std::to_string(index));
+            }
+            return arr[index];
+        }
+
         void clear(){
             m_size = 0;
+        }
+        size_t find(T element) const{
+            for(size_t i = 0; i < m_size; i++){
+                if (arr[i] == element){
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        void remove_at(size_t index){
+            if (index >= m_size || index < 0) {
+                throw std::out_of_range("Index is out of bounds of the static vector: " + std::to_string(index));
+            }
+            if (index == m_size-1){
+                pop_back();
+                return;
+            }
+            
+            for (size_t i = index; i < m_size-1; i++){
+                arr[i] = arr[i+1];
+            }
+            
+            m_size--;
         }
 
 
