3750ff1a7e6024f0ad9b692ddcf7e44b7066762c
tfergus23 <tomfergus22@gmail.com>
2025-03-27 13:20:14 -0500

    Add load_dotenv function for .env files

---
+++ include/tflib/dotenv.h
@@ -0,0 +1,4 @@
+namespace tflib{
+    void load_dotenv();
+    void load_dotenv(const char* path_to_env_file);
+}
---
+++ tflib/dotenv.cpp
@@ -0,0 +1,30 @@
+#include "../include/tflib/dotenv.h"
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+void tflib::load_dotenv(){
+    load_dotenv("./.env");
+}
+
+void tflib::load_dotenv(const char* path_to_env_file){
+    std::ifstream file(path_to_env_file);
+
+    if(!file.good()){
+        std::cerr << "Couldn't open env file: " + std::string(path_to_env_file) << '\n';
+        return;
+    }
+
+    for (std::string line; std::getline(file, line);){
+        size_t equals = line.find("=");
+        if (equals == std::string::npos){
+            continue;
+        }
+
+        std::string name = line.substr(0,equals);
+        std::string value = line.substr(equals+1, line.size()-equals);
+        setenv(name.c_str(), value.c_str(), 1);
+    }
+}
