604072ab49cde16150ce0da9340d6f22c531a573
tfergus23 <tomfergus22@gmail.com>
2025-04-24 12:55:23 -0500

    Fix double namespace

--- include/tflib/dotenv.h
+++ include/tflib/dotenv.h
@@ -4,5 +4,5 @@ namespace tflib{
     void load_dotenv();
     void load_dotenv(const char* path_to_env_file);
 
-    std::string tflib::get_env(std::string_view name);
+    std::string get_env(std::string_view name);
 }
